7a9391df8da294008cfe0ee29566edbf29a8db20
Alex Orga <alexorga.dev@gmail.com>
2018-05-15 20:23:32 -0500

    printing cards to page

--- javascripts/data.js
+++ javascripts/data.js
@@ -1,26 +1,61 @@
-const loadLocations = require('./locations');
-const loadExInfo = require('./ex');
+// const loadLocations = require('./locations');
+// const loadExInfo = require('./ex');
 const dom = require('./dom');
-const events = require('./events');
+// const events = require('./events');
 
-const whenLocationsLoad = (data) => {
-  const locations = data.locations;
-  $('#locations').html(dom.locationDom(locations));
-  events.addButtonEvents();
-  events.getSearch();
+// const whenLocationsLoad = (data) => {
+//   const locations = data.locations;
+//   $('#locations').html(dom.locationDom(locations));
+//   events.addButtonEvents();
+//   events.getSearch();
+// };
+
+// const whenExInfoLoads = (data) => {
+//   $('#exInfo').html(dom.exDom(data.ex));
+// };
+
+// const loadFail = () => {
+//   console.error('Whoops! There was an error, try again!');
+// };
+
+const exLoad = () => {
+  return new Promise ((resolve, reject) => {
+    $.get('../db/ex.json')
+      .done((data) => {
+        const exes = data.exes;
+        resolve(exes);
+      })
+      .fail((error) => {
+        reject('oops!', error);
+      });
+  });
 };
 
-const whenExInfoLoads = (data) => {
-  $('#exInfo').html(dom.exDom(data.ex));
-};
-
-const loadFail = () => {
-  console.error('Whoops! There was an error, try again!');
+const locationLoad = () => {
+  return new Promise((resolve, reject) => {
+    $.get('../db/locations.json')
+      .done((data) => {
+        const locations = data.locations;
+        resolve(locations);
+      })
+      .fail((error) => {
+        reject('oops!', error);
+      });
+  });
 };
 
 const initializer = () => {
-  loadLocations(whenLocationsLoad, loadFail);
-  loadExInfo(whenExInfoLoads, loadFail);
+  exLoad().then((exes) => {
+    dom.exDom(exes);
+    locationLoad().then((locations) => {
+      dom.locationDom(locations, exes);
+    });
+  });
+  // locationLoad().then((locations) => {
+  //   dom.locationDom(locations);
+  // });
 };
 
-module.exports = initializer;
+module.exports = {
+  initializer,
+};
--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,4 +1,31 @@
 
+// const locationDom = (locations) => {
+//   let domString = '';
+//   locations.forEach((location) => {
+//     domString += `<div class="location-card ${location.timeOfDay} col-lg-6">`;
+//     domString +=    `<img src="${location.image}">`;
+//     domString +=    `<h3>${location.name}</h3>`;
+//     domString +=    `<h4>${location.address}</h4>`;
+//     domString +=    `<p><strong>Time of Day: <strong> ${location.timeOfDay}</p>`;
+//     domString +=  `</div>`;
+//   });
+//   return domString;
+// };
+
+// const exDom = (exInfo) => {
+//   let domString = '';
+//   domString +=  `<div>`;
+//   domString +=    `<img src="${exInfo.image}">`;
+//   domString +=    `<h3>${exInfo.name}</h3>`;
+//   domString +=    `<h4>${exInfo.age}</h4>`;
+//   domString +=    `<h4>Flaws:</h4>`;
+//   exInfo.flaws.forEach((flaw) => {
+//     domString +=   `<p>${flaw}</p>`;
+//   });
+//   domString +=  `</div>`;
+//   return domString;
+// };
+
 const locationDom = (locations) => {
   let domString = '';
   locations.forEach((location) => {
@@ -9,21 +36,21 @@ const locationDom = (locations) => {
     domString +=    `<p><strong>Time of Day: <strong> ${location.timeOfDay}</p>`;
     domString +=  `</div>`;
   });
-  return domString;
+  $('#locations').html(domString);
 };
 
 const exDom = (exInfo) => {
   let domString = '';
-  domString +=  `<div>`;
-  domString +=    `<img src="${exInfo.image}">`;
-  domString +=    `<h3>${exInfo.name}</h3>`;
-  domString +=    `<h4>${exInfo.age}</h4>`;
-  domString +=    `<h4>Flaws:</h4>`;
-  exInfo.flaws.forEach((flaw) => {
-    domString +=   `<p>${flaw}</p>`;
+  exInfo.forEach((ex) => {
+    domString +=  `<div>`;
+    domString +=    `<img src="${ex.image}">`;
+    domString +=    `<h3>${ex.name}</h3>`;
+    domString +=    `<h4>${ex.age}</h4>`;
+    domString +=    `<h4>Flaws:</h4>`;
+    domString +=    `<p>${ex.flaws}</p>`;
+    domString +=  `</div>`;
   });
-  domString +=  `</div>`;
-  return domString;
+  $('#exInfo').html(domString);
 };
 
 module.exports = {
