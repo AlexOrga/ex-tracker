fdf0cbf7bb5d21b98b90b1cf49d2f4337e1d3597
Alex Orga <alexorga.dev@gmail.com>
2018-05-16 20:29:37 -0500

    added some styling

--- javascripts/data.js
+++ javascripts/data.js
@@ -1,23 +1,5 @@
-// const loadLocations = require('./locations');
-// const loadExInfo = require('./ex');
 const dom = require('./dom');
 const events = require('./events');
-// const events = require('./events');
-
-// const whenLocationsLoad = (data) => {
-//   const locations = data.locations;
-//   $('#locations').html(dom.locationDom(locations));
-//   events.addButtonEvents();
-//   events.getSearch();
-// };
-
-// const whenExInfoLoads = (data) => {
-//   $('#exInfo').html(dom.exDom(data.ex));
-// };
-
-// const loadFail = () => {
-//   console.error('Whoops! There was an error, try again!');
-// };
 
 const exLoad = () => {
   return new Promise ((resolve, reject) => {
@@ -45,10 +27,6 @@ const locationLoad = () => {
   });
 };
 
-// const singleEx = (event) => {
-//   alert(e.target.name);
-// };
-
 const initializer = () => {
   events.addButtonEvents();
   events.getSearch();
@@ -61,13 +39,9 @@ const initializer = () => {
     });
 
   });
-  // locationLoad().then((locations) => {
-  //   dom.locationDom(locations);
-  // });
 };
 
 module.exports = {
   initializer,
   exLoad,
-  // singleEx,
 };
--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,31 +1,3 @@
-
-// const locationDom = (locations) => {
-//   let domString = '';
-//   locations.forEach((location) => {
-//     domString += `<div class="location-card ${location.timeOfDay} col-lg-6">`;
-//     domString +=    `<img src="${location.image}">`;
-//     domString +=    `<h3>${location.name}</h3>`;
-//     domString +=    `<h4>${location.address}</h4>`;
-//     domString +=    `<p><strong>Time of Day: <strong> ${location.timeOfDay}</p>`;
-//     domString +=  `</div>`;
-//   });
-//   return domString;
-// };
-
-// const exDom = (exInfo) => {
-//   let domString = '';
-//   domString +=  `<div>`;
-//   domString +=    `<img src="${exInfo.image}">`;
-//   domString +=    `<h3>${exInfo.name}</h3>`;
-//   domString +=    `<h4>${exInfo.age}</h4>`;
-//   domString +=    `<h4>Flaws:</h4>`;
-//   exInfo.flaws.forEach((flaw) => {
-//     domString +=   `<p>${flaw}</p>`;
-//   });
-//   domString +=  `</div>`;
-//   return domString;
-// };
-
 const locationDom = (locations, exes) => {
   let domString = '';
   locations.forEach((location) => {
